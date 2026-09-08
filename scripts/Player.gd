f8c8e69771d0341a256d0925ea332920c819b22b
Salvatore Costanzo <salvatore@estudante.ufscar.br>
2026-09-07 23:35:59 -0300

    paralax test update

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -10,10 +10,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Pulo.
+	var direction := Input.get_axis("left", "right")
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if is_on_floor() == false:
+		if direction > 0:
+			animated.flip_h = false
+		elif direction < 0:
+			animated.flip_h = true
+		
 	#Movimentação.
-	var direction := Input.get_axis("left", "right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
