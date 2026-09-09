3b9f64df49ad017e15c247b6f4d9ca7cabd9afb3
Salvatore Costanzo <salvatore@estudante.ufscar.br>
2026-09-08 22:57:24 -0300

    Script de camera adicionado

---
+++ scripts/camera.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+var target: Node2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_target()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position = target.position
+
+func get_target():
+		#localiza a tag Player para a camera
+	var nodes = get_tree().get_nodes_in_group("Player")
+	if nodes.size() == 0:
+		push_error("deu ruim na localização de player")
+		return #encerra o processo
+	target = nodes[0] #atribui o primeiro alvo do vetor Players
